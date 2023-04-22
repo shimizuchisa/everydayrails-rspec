fe90ca9a0085af53f6c7d2e3c730bbb54f1a4932
shimizuchisa <112614370+shimizuchisa@users.noreply.github.com>
2023-04-22 08:13:50 +0000

    add spec project_controller_update

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -37,68 +37,89 @@ RSpec.describe ProjectsController, type: :controller do
   #   end
   # end
 
-  describe "show" do
-    # 認可されたユーザーとして
-    context "as an authorized user" do
+  # describe "show" do
+  #   # 認可されたユーザーとして
+  #   context "as an authorized user" do
+  #     before do
+  #       @user = FactoryBot.create(:user)
+  #       @project = FactoryBot.create(:project, owner: @user)
+  #     end
+
+  #     # 正常にレスポンスを返すこと
+  #     it "responds successfully" do
+  #       sign_in @user
+  #       get :show, params: {id: @project.id}
+  #       expect(response).to be_successful
+  #     end
+  #   end
+
+  #   # 認可されていないユーザーとして
+  #   context "as an unauthoprized user" do
+  #     before do
+  #       @user = FactoryBot.create(:user)
+  #       @project = FactoryBot.create(:project, owner: @user)
+  #       @other = FactoryBot.create(:user)
+  #     end
+
+  #     # ダッシュボードにリダイレクトすること
+  #     it "responds unsuccessfully" do
+  #       sign_in @other
+  #       get :show, params: {id: @project.id}
+  #       expect(response).to redirect_to root_path
+  #     end
+  #   end
+  # end
+
+  # describe "create" do
+  #   #認証済みのユーザーとして
+  #   context "as an authorized user" do
+  #     before do
+  #       @user = FactoryBot.create(:user)
+  #     end
+
+  #     # プロジェクトを追加できること
+  #     it "adds a project" do
+  #       sign_in @user
+  #       #attributes_for(:モデル)・・・ファクトリからテスト用の属性値をハッシュとして作成
+  #       project_params = FactoryBot.attributes_for(:project)
+  #       expect {
+  #         post :create, params: { project: project_params }
+  #       }.to change(@user.projects, :count).by(1)
+  #     end
+  #   end
+
+  #   # ゲストとして
+  #   context "as a guest" do
+  #     #302レスポンスを返すこと
+  #     it "returnes a 302 response" do
+  #       project_params = FactoryBot.attributes_for(:project)
+  #       post :create, params: { project: project_params}
+  #       expect(response).to have_http_status "302"
+  #     end
+
+  #     #3サインイン画面にリダイレクトすること
+  #     it "redirect to sign_in page" do
+  #       project_params = FactoryBot.attributes_for(:project)
+  #       post :create, params: { project: project_params }
+  #       expect(response).to redirect_to "/users/sign_in"
+  #     end
+  #   end
+  # end
+
+  describe "update" do
+    #認可されたユーザーとして
+    context "as a authorized user" do
       before do
         @user = FactoryBot.create(:user)
         @project = FactoryBot.create(:project, owner: @user)
       end
-
-      # 正常にレスポンスを返すこと
-      it "responds successfully" do
+      #自分のプロジェクトは編集できる
+      it "updates a project" do
         sign_in @user
-        get :show, params: {id: @project.id}
-        expect(response).to be_successful
+        project_params = FactoryBot.attributes_for(:project, name: "New projett name")
+        patch :update, params: {id: @project, project: project_params}
+        expect(@project.reload.name).to eq "New projett name"
       end
-    end
-
-    # 認可されていないユーザーとして
-    context "as an unauthoprized user" do
-      before do
-        @user = FactoryBot.create(:user)
-        @project = FactoryBot.create(:project, owner: @user)
-        @other = FactoryBot.create(:user)
-      end
-
-      # ダッシュボードにリダイレクトすること
-      it "responds unsuccessfully" do
-        sign_in @other
-        get :show, params: {id: @project.id}
-        expect(response).to redirect_to root_path
-      end
-    end
-  end
-
-  describe "create" do
-    #認証済みのユーザーとして
-    context "as an authorized user" do
-      before do
-        @user = FactoryBot.create(:user)
-      end
-
-      # プロジェクトを追加できること
-      it "add a project" do
-        sign_in @user
-        #attributes_for(:モデル)・・・ファクトリからテスト用の属性値をハッシュとして作成
-        project_params = FactoryBot.attributes_for(:project)
-        expect {
-          post :create, params: { project: project_params }
-        }.to change(@user.projects, :count).by(1)
-      end
-    end
-
-    # ゲストとして
-    context "as an guest" do
-      #302レスポンスを返すこと
-      it "returnes response 302 " do
-        project_params = FactoryBot.attributes_for(:project)
-        post :create, params: { project: project_params}
-        expect(response).to have_http_status "302"
-      end
-
-      #3サインイン画面にリダイレクトすること
-      it
     end
   end
 end
