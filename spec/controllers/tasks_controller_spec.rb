8d013b7b086724319825559a4a86a313397726e1
shimizuchisa <112614370+shimizuchisa@users.noreply.github.com>
2023-04-27 08:54:09 +0000

    add rspec system_project

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -20,28 +20,41 @@ RSpec.describe TasksController, type: :controller do
 
   describe "create" do
      # タスクが作成できること
-    it "add a task" do
+    # it "add a task" do
+    #   sign_in @user
+    #   new_task = {name: "New test task"}
+    #   expect{
+    #     post :create, params: {project_id: @project.id, task: new_task}
+    #   }.to change(@project.tasks, :count).by(1)
+    # end
+
+    # # JSON 形式でレスポンスを返すこと
+    # it "responses with JSON formatted output" do
+    #   new_task = { name: "New test task" }
+    #   sign_in @user
+    #   post :create, format: :json,
+    #     params: { project_id: @project.id, task: new_task }
+    #   expect(response.content_type).to include "application/json"
+    # end
+
+    # 新しいタスクをプロジェクトに追加すること
+    it "adds a new task to project" do
       sign_in @user
-      new_task = {name: "New test task"}
+      task_params = {name: "Add new task"}
       expect{
-        post :create, params: {project_id: @project.id, task: new_task}
+        post :create, format: :json,
+          params: {project_id: @project.id, task: task_params}
       }.to change(@project.tasks, :count).by(1)
     end
 
-    # JSON 形式でレスポンスを返すこと
-    it "responses with JSON formatted output" do
-      new_task = { name: "New test task" }
-      sign_in @user
-      post :create, format: :json,
-        params: { project_id: @project.id, task: new_task }
-      expect(response.content_type).to include "application/json"
-    end
-    
-    # 新しいタスクをプロジェクトに追加すること
-    
-    
     # 認証を要求すること
-    
-    
+    it "requires authentication" do
+      task_params = {name: "Add new task"}
+      expect{
+        post :create, format: :json,
+          params: {project_id: @project.id, task: task_params}
+      }.to_not change(@project.tasks, :count)
+      expect(response).to_not be_successful
+    end
   end
 end
